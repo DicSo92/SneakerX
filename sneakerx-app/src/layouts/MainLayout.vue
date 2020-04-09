179221ff89c5617e97b5973f081e9f21674143c8
Luzzi Charly <luzzi.charly@gmail.com>
2020-04-09 18:13:46 +0200

    Update layout links

--- sneakerx-app/src/layouts/MainLayout.vue
+++ sneakerx-app/src/layouts/MainLayout.vue
@@ -18,91 +18,48 @@
               show-if-above
               bordered
               content-class="bg-grey-1">
+
       <q-list>
         <q-item-label header
                       class="text-grey-8">
           Essential Links
         </q-item-label>
+
         <EssentialLink v-for="link in essentialLinks"
                        :key="link.title"
                        v-bind="link"/>
-
-        <q-item clickable
-                tag="a"
-                :to="{name: 'auth'}">
-          <q-item-section avatar>
-            <q-icon name="code"/>
-          </q-item-section>
-
-          <q-item-section>
-            <q-item-label>Login / Register</q-item-label>
-            <q-item-label caption>
-              login.register
-            </q-item-label>
-          </q-item-section>
-        </q-item>
-
       </q-list>
     </q-drawer>
 
     <q-page-container>
-      <router-view />
+      <router-view/>
     </q-page-container>
+
   </q-layout>
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink'
+    import EssentialLink from 'components/EssentialLink'
 
-export default {
-  name: 'MainLayout',
+    export default {
+        name: 'MainLayout',
 
-  components: {
-    EssentialLink
-  },
+        components: {
+            EssentialLink
+        },
 
-  data () {
-    return {
-      leftDrawerOpen: false,
-      essentialLinks: [
-        {
-          title: 'Docs',
-          caption: 'quasar.dev',
-          icon: 'school',
-          link: 'https://quasar.dev'
-        },
-        {
-          title: 'Github',
-          caption: 'github.com/quasarframework',
-          icon: 'code',
-          link: 'https://github.com/quasarframework'
-        },
-        {
-          title: 'Discord Chat Channel',
-          caption: 'chat.quasar.dev',
-          icon: 'chat',
-          link: 'https://chat.quasar.dev'
-        },
-        {
-          title: 'Forum',
-          caption: 'forum.quasar.dev',
-          icon: 'record_voice_over',
-          link: 'https://forum.quasar.dev'
-        },
-        {
-          title: 'Twitter',
-          caption: '@quasarframework',
-          icon: 'rss_feed',
-          link: 'https://twitter.quasar.dev'
-        },
-        {
-          title: 'Facebook',
-          caption: '@QuasarFramework',
-          icon: 'public',
-          link: 'https://facebook.quasar.dev'
+        data() {
+            return {
+                leftDrawerOpen: false,
+                essentialLinks: [
+                    {
+                        title: 'Login / Register',
+                        caption: 'login.register',
+                        icon: 'code',
+                        link: 'auth'
+                    },
+                ]
+            }
         }
-      ]
     }
-  }
-}
 </script>
