7c5f5cf80463a498b67144d514b5d01b3875f15a
Luzzi Charly <luzzi.charly@gmail.com>
2020-04-12 01:04:23 +0200

    Add visible columns selector

--- sneakerx-app/src/components/admin/users/UsersList.vue
+++ sneakerx-app/src/components/admin/users/UsersList.vue
@@ -7,7 +7,33 @@
       row-key="name"
       selection="single"
       :selected.sync="selected"
-    />
+      :visible-columns="visibleColumns"
+    >
+
+      <template v-slot:top>
+        <img
+          style="height: 50px; width: 50px"
+          src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg"
+        >
+
+        <q-space />
+
+        <q-select
+          v-model="visibleColumns"
+          multiple
+          outlined
+          dense
+          options-dense
+          :display-value="$q.lang.table.columns"
+          emit-value
+          map-options
+          :options="columns"
+          option-value="name"
+          options-cover
+          style="min-width: 150px"
+        />
+      </template>
+    </q-table>
   </div>
 </template>
 
@@ -20,6 +46,7 @@
         data() {
             return {
                 selected: [],
+                visibleColumns: ['id', 'name', 'email', 'is_admin', 'created_at'],
                 columns: [
                     {
                         name: 'id', required: true, label: 'ID',
@@ -31,8 +58,8 @@
                     {name: 'email', label: 'Email', field: 'email', sortable: true},
                     {name: 'email_verified_at', label: 'Verified At', field: 'email_verified_at'},
                     {name: 'is_admin', label: 'Admin', field: 'is_admin'},
-                    {name: 'created_at', label: 'Created', field: 'created_at'},
-                    {name: 'updated_at', label: 'Updated', field: 'updated_at', sortable: true},
+                    {name: 'created_at', label: 'Created At', field: 'created_at'},
+                    {name: 'updated_at', label: 'Updated At', field: 'updated_at', sortable: true},
                 ],
             }
         }
