21984a2ac4472a68cdeccfaa45ca643bbf5ace43
Luzzi Charly <luzzi.charly@gmail.com>
2020-04-11 18:17:47 +0200

    Remove mutations logs

--- sneakerx-app/src/store/auth/mutations.js
+++ sneakerx-app/src/store/auth/mutations.js
@@ -1,10 +1,8 @@
 export function someMutation (/* state */) {
 }
 export const setIsLoggedIn = (state, status) => {
-  console.log('set IsLoggedIn')
   state.isLoggedIn = status
 }
 export const setUser = (state, user) => {
-  console.log('set user')
   state.user = user
 }
