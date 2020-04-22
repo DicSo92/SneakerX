8ea4c8705881c1a3d324791c8f3d9cd7ef0525f1
Luzzi Charly <luzzi.charly@gmail.com>
2020-04-23 00:52:44 +0200

    Create Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
-        $this->call(UsersTableSeeder::class);
+
+        // $this->call(UsersTableSeeder::class);
+
+        $this->call(BrandsTableSeeder::class);
     }
 }
