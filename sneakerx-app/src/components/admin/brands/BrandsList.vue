d0a808bc795c0147955b4780a47c44fc614e89bd
Luzzi Charly <luzzi.charly@gmail.com>
2020-04-25 16:17:13 +0200

    Make list ~responsive

--- sneakerx-app/src/components/admin/brands/BrandsList.vue
+++ sneakerx-app/src/components/admin/brands/BrandsList.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="brandList column flex-center">
+
     <div class="full-width flex justify-between q-mb-sm">
-      <div class="spinnerContainer">
+      <div id="spinnerContainer">
         <q-spinner color="primary"
                    size="3em"
                    v-if="loading"/>
       </div>
       <q-btn label="New Brand" @click="addBrand" color="primary"/>
     </div>
-    <q-list bordered>
+
+    <q-list bordered id="list">
       <q-item clickable v-ripple v-for="brand in brands">
         <q-item-section avatar>
           <q-avatar rounded>
@@ -30,22 +32,23 @@
         </q-item-section>
       </q-item>
     </q-list>
+
   </div>
 </template>
 
 <script>
     import bus from '../../../utils/bus.js'
-    import { QSpinnerGears } from 'quasar'
+    import {QSpinnerGears} from 'quasar'
 
     export default {
         name: "BrandsList",
-        data () {
+        data() {
             return {
                 brands: {},
                 loading: true
             }
         },
-        created () {
+        created() {
             this.getBrands()
         },
         mounted() {
@@ -53,59 +56,64 @@
                 this.getBrands()
             })
         },
-         methods: {
-             getBrands () {
-                 this.loading = true
-                 this.$axios.get('/api/admin/brands')
-                     .then(response => {
-                         console.log(response)
-                         this.brands = response.data
-                         this.loading = false
-                     })
-                     .catch(error => {
-                         console.log('error')
-                         console.log(error)
-                         this.loading = false
-                     })
-             },
-             deleteBrand (id) {
-                 this.$q.loading.show({
-                     message: `Deleting Brand...`
-                 })
-                 this.$axios.delete(`/api/admin/brands/${id}`)
-                     .then(response => {
-                         console.log(response)
-                         this.brands.splice(this.brands.findIndex(brand => brand.id === id), 1)
-                         this.hideLoading('Brand Deleted')
-                     })
-                     .catch(error => {
-                         console.log(error)
-                         this.hideLoading('An error occurred : ' + error.message)
-                     })
-             },
-             hideLoading (message) {
-                 this.$q.loading.show({
-                     spinner: QSpinnerGears,
-                     spinnerColor: 'red',
-                     messageColor: 'black',
-                     backgroundColor: 'yellow',
-                     message: message
-                 })
+        methods: {
+            getBrands() {
+                this.loading = true
+                this.$axios.get('/api/admin/brands')
+                    .then(response => {
+                        console.log(response)
+                        this.brands = response.data
+                        this.loading = false
+                    })
+                    .catch(error => {
+                        console.log('error')
+                        console.log(error)
+                        this.loading = false
+                    })
+            },
+            deleteBrand(id) {
+                this.$q.loading.show({
+                    message: `Deleting Brand...`
+                })
+                this.$axios.delete(`/api/admin/brands/${id}`)
+                    .then(response => {
+                        console.log(response)
+                        this.brands.splice(this.brands.findIndex(brand => brand.id === id), 1)
+                        this.hideLoading('Brand Deleted')
+                    })
+                    .catch(error => {
+                        console.log(error)
+                        this.hideLoading('An error occurred : ' + error.message)
+                    })
+            },
+            hideLoading(message) {
+                this.$q.loading.show({
+                    spinner: QSpinnerGears,
+                    spinnerColor: 'red',
+                    messageColor: 'black',
+                    backgroundColor: 'yellow',
+                    message: message
+                })
 
-                 this.timer = setTimeout(() => {
-                     this.$q.loading.hide()
-                     this.timer = void 0
-                 }, 2000)
-             },
-             addBrand () {
-                 bus.$emit('showAddModalBrand', true)
-             }
-         }
+                this.timer = setTimeout(() => {
+                    this.$q.loading.hide()
+                    this.timer = void 0
+                }, 2000)
+            },
+            addBrand() {
+                bus.$emit('showAddModalBrand', true)
+            }
+        }
     }
 </script>
 
 <style scoped lang="scss">
-    .spinnerContainer {
-        min-width: 10px;
-    }
+  #spinnerContainer {
+    min-width: 10px;
+  }
+
+  #list {
+    width: 700px;
+    max-width: 80vw;
+  }
 </style>
