2d575dd7d398ad70f11dee2bc2eab9dfa9ea7943
Luzzi Charly <luzzi.charly@gmail.com>
2020-04-28 16:49:07 +0200

    Mrc / Migrations / Relations

---
+++ app/Product.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name', 'description', 'image', 'price', 'active', 'brand_id'
+    ];
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+    
+    // JoinTables:
+    // products_colors : [ product_id: ..., colors: [ { color: '#555', available: true}, {...}, ... ] ]
+    // products_shoe_sizes : [ product_id: ..., sizes: [ { size: 42, available: true}, {...}, ... ] ]
+}
