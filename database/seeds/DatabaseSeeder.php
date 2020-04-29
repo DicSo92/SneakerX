d1f14a559428abb20692a62e4c5757c5d3ab050b
Luzzi Charly <luzzi.charly@gmail.com>
2020-04-29 21:42:38 +0200

    Create products Seeder with Images

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,10 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-//        $this->call(UserSeeder::class);
-
         $this->call(UsersTableSeeder::class);
 
         $this->call(BrandsTableSeeder::class);
+
+        $this->call(ProductsTableSeeder::class);
+
+//        $this->call(ProductsImagesTableSeeder::class);
     }
 }
