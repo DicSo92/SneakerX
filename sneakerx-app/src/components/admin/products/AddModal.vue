560d20bbdb8b6dd4c2ed401ece03b5c7b4db0cfb
Luzzi Charly <luzzi.charly@gmail.com>
2020-05-03 22:17:02 +0200

    Refractor sizes code to SizesPicker component

--- sneakerx-app/src/components/admin/products/AddModal.vue
+++ sneakerx-app/src/components/admin/products/AddModal.vue
@@ -48,51 +48,14 @@
 
               <div class="row q-mt-md">
                 <ColorsPicker :colorsProps="colors" class="col"
-                              @colorsChange="changeColorsOnEmit">
+                              @colorsChange="colors = $event">
                 </ColorsPicker>
 
                 <q-separator vertical class="q-mx-md"/>
 
-                <div class="col column">
-                  <div class="flex q-mb-sm q-pb-sm" style="border-bottom: solid 1px #eeeeee;">
-                    <q-icon name="colorize" size="sm" color="purple"/>
-                    <div class="text-subtitle2 q-ml-xs">Sizes* :</div>
-                    <div class="text-caption text-negative q-ml-sm" v-if="!sizesAsc.length">No sizes selected*</div>
-                    <q-space/>
-                  </div>
-                  <q-select
-                    filled
-                    dense
-                    v-model="sizes"
-                    multiple
-                    :options="sizeOptions"
-                    option-label="size"
-                    label="Choose Sizes"
-                    counter
-                    hint="Counter Sizes"
-                    class="full-width q-mb-md">
-
-                    <template v-if="sizes.length" v-slot:append>
-                      <q-icon name="cancel" @click.stop="sizes = []" class="cursor-pointer"/>
-                    </template>
-                  </q-select>
-                  <div class="flex">
-                    <q-chip v-for="(size, index) in sizesAsc" :key="size.size"
-                            dense
-                            removable
-                            clickable
-                            :color="size.available ? 'green-6' : 'red-7'"
-                            :selected.sync="sizes[index].available"
-                            class="text-bold"
-                            text-color="white"
-                            icon="warning"
-                            @remove="sizes.splice(index, 1)">
-<!--                      <q-avatar color="red" text-color="white">{{size.size}}</q-avatar>-->
-                      {{size.size}}
-                    </q-chip>
-                  </div>
-                </div>
-
+                <SizesPicker :sizesProps="sizes" class="col"
+                             @sizesChange="sizes = $event">
+                </SizesPicker>
               </div>
             </div>
           </div>
@@ -117,7 +80,6 @@
             />
           </div>
 
-
           <div class="column">
             <q-toggle v-model="directActive" label="Check to instantly publish product on catalog. (Not recommended)"/>
             <div>
@@ -130,7 +92,6 @@
               <q-btn label="Cancel" @click="showEdit = false" color="primary" flat class="q-ml-sm"/>
             </div>
 
-
           </div>
         </q-form>
       </q-card-section>
@@ -141,6 +102,7 @@
 <script>
     import draggable from "vuedraggable";
     import ColorsPicker from "./ColorsPicker.vue";
+    import SizesPicker from "./SizesPicker.vue";
 
     export default {
         name: "AddModal",
@@ -149,26 +111,24 @@
         ],
         components: {
             draggable,
+            SizesPicker,
             ColorsPicker
         },
         data() {
             return {
                 showEdit: false,
-                name: '',
-                description: '',
-                bannerFile: null,
-                imageFile: null,
-
                 loading: false,
 
                 image: true,
+                name: '',
+                description: '',
                 brandSelected: null,
+                colors: [],
+                sizes: [],
                 directActive: false,
 
-                colors: [],
-
-                sizes: [],
-                sizeOptions: [{size: 34, available: true}, {size: 35, available: true}, {size: 36, available: true}, {size: 37, available: true}, {size: 38, available: true}, {size: 39, available: true}, {size: 40, available: true}, {size: 41, available: true}, {size: 42, available: true}, {size: 43, available: true}, {size: 44, available: true}, {size: 45, available: true}, {size: 46, available: true}, {size: 47, available: true}, {size: 48, available: true}, {size: 49, available: true}, {size: 50, available: true}, {size: 51, available: true}, {size: 52, available: true}]
+                bannerFile: null,
+                imageFile: null,
             }
         },
         mounted() {
@@ -178,23 +138,8 @@
         },
         watch: {},
         computed: {
-            sizesAsc() {
-                return this.sizes.sort(this.sizesAscComparison)
-            }
         },
         methods: {
-            sizesAscComparison(a, b) {
-                let comparison = 0;
-                if (a.size > b.size) {
-                    comparison = 1;
-                } else if (a.size < b.size) {
-                    comparison = -1;
-                }
-                return comparison;
-            },
-            changeColorsOnEmit(colors) {
-                this.colors = colors
-            },
             addBrand() {
                 console.log('Fake Submit');
             },
