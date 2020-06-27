151d6fca407d3434581737d82440903de9c934db
Luzzi Charly <luzzi.charly@gmail.com>
2020-06-27 02:47:27 +0200

    Build (home access)

--- sneakerx-app/src/router/routes.js
+++ sneakerx-app/src/router/routes.js
@@ -8,7 +8,7 @@ const routes = [
         path: '',
         name: 'home',
         component: () => import('pages/Home.vue'),
-        meta: { requiresAuth: true }
+        // meta: { requiresAuth: true }
       },
       {
         path: '/auth',
